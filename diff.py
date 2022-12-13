2714cdb859f4d4d9f4535a9729ad3ee6fb62e743
Sam Hill <samuhill@gmail.com>
2022-12-12 23:07:35 -0500

    diff in the actions

--- diff.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-
-fetch_result = subprocess.check_output(["git", "fetch", "origin", "main"])
-diff_result = str(subprocess.check_output(["git", "diff", f"origin/main"]))
-print(diff_result)
