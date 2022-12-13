5b8623580afaa8a6ea2b86fa1f512d3dcd984fed
Sam Hill <samuhill@gmail.com>
2022-12-12 22:38:09 -0500

    get branch name dynamically

--- diff.py
+++ diff.py
@@ -1,5 +1,7 @@
 import subprocess
+import sys
 
-fetch_result = subprocess.check_output(["git", "fetch", "origin", "master"])
-diff_result = str(subprocess.check_output(["git", "diff", "origin/master"]))
+
+fetch_result = subprocess.check_output(["git", "fetch", "origin", "main"])
+diff_result = str(subprocess.check_output(["git", "diff", f"origin/main"]))
 print(diff_result)
